ae0f6a30f0df2680cd98e4f4ee693a2e5b85e17e
Pe6e3 <xpe6e3@gmail.com>
2023-04-01 13:09:10 +0600

    Добавил модель Статус

---
+++ ToDo-List/Models/Status.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class Status
+    {
+        [Key] public int StatusId { get; set; }
+
+        [Display(Name = "Статус задачи")]
+        public string? StatusName { get; set; }
+    }
+}
