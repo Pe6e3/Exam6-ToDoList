e334fe73c2beba873fb96676f5af218a99b1a03e
Pe6e3 <xpe6e3@gmail.com>
2023-04-01 11:27:07 +0600

    Обновленные миграции (не приложил в прошлый коммит)

--- ToDo-List/Migrations/20230401052528_add field PriorityId to model MyTask, and virtual to field Priority.Designer.cs
+++ ToDo-List/Migrations/20230401052528_add field PriorityId to model MyTask, and virtual to field Priority.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoList.Data;
 
@@ -11,9 +12,11 @@ using ToDoList.Data;
 namespace ToDo_List.Migrations
 {
     [DbContext(typeof(ToDoListContext))]
-    partial class ToDoListContextModelSnapshot : ModelSnapshot
+    [Migration("20230401052528_add field PriorityId to model MyTask, and virtual to field Priority")]
+    partial class addfieldPriorityIdtomodelMyTaskandvirtualtofieldPriority
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
