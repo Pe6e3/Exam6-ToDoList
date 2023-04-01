73df8610f2e4bc3717466e69dc5fb894f5385f08
Pe6e3 <xpe6e3@gmail.com>
2023-04-01 10:50:45 +0600

    Создал контроллер Задач

---
+++ ToDo-List/Controllers/MyTasksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDo_List.Controllers
+{
+    public class MyTasksController : Controller
+    {
+        private readonly ToDoListContext _db;
+        public MyTasksController(ToDoListContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<MyTask> myTasks = _db.Tasks.ToList();
+            return View(myTasks);
+        }
+    }
+}
